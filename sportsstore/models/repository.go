6fdf626abef006114aea33b675519f0b8c0b6243
somotto <stephenonyango4783@gmail.com>
2024-10-31 17:49:13 +0300

    feat: initial file

---
+++ sportsstore/models/repository.go
@@ -0,0 +1,13 @@
+package models
+
+type Repository interface {
+	GetProduct(id int) Product
+
+	GetProducts() []Product
+
+	GetProductPage(page, pageSize int) (products []Product, totalAvailable int)
+
+	GetCategories() []Category
+
+	Seed()
+}
